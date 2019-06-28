e788086552400fb36b12f58d5841591f0eaeb678
Giovanni Gonzaga <giovanni@buildo.io>
2019-06-28 18:36:53 +0200

    show a few typings improvements in the dtslint file

--- dtslint/ts3.5/index.ts
+++ dtslint/ts3.5/index.ts
@@ -1,16 +1,35 @@
 import * as FR from '../../src';
+import * as R from 'ramda';
+
+// fromPairs
 
 FR.fromPairs([]); // $ExpectType Record<string, unknown>
+
+R.fromPairs([['a', 1], ['b', 2]]); // $ExpectType { [index: string]: number; }
 FR.fromPairs([['a', 1], ['b', 2]]); // $ExpectType Record<"a" | "b", number>
 
+// toPairs
+
+R.toPairs({}); // $ExpectType [string, unknown][]
 FR.toPairs({}); // $ExpectType [never, never][]
+
+R.toPairs({ a: 1, b: false }); // $ExpectType [string, number | boolean][]
 FR.toPairs({ a: 1, b: false }); // $ExpectType ["a" | "b", number | boolean][]
+
+// xprod
 
 FR.xprod([], []); // $ExpectType [never, never][]
 FR.xprod(['a', 1], [true, new Date()]); // $ExpectType [string | number, boolean | Date][]
 FR.xprod(['a', 1])([true, new Date()]); // $ExpectType [string | number, boolean | Date][]
 
+// adjust
+
 FR.adjust(1, a => a + a, ['a']); // $ExpectType string[]
+
+R.adjust(1)(a => a + a, ['a']); // $ExpectError
 FR.adjust(1)(a => a + a, ['a']); // $ExpectType string[]
+
 FR.adjust(1, (a: string) => a + a)(['a']); // $ExpectType string[]
+
+R.adjust(1)((a: string) => a + a)(['a']); // $ExpectError
 FR.adjust(1)((a: string) => a + a)(['a']); // $ExpectType string[]
