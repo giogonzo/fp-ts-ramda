f2efe0563eabaada852f1d2163da771c1d514045
Giovanni Gonzaga <giovanni@buildo.io>
2019-06-28 12:17:54 +0200

    add missing fromPairs return type

--- src/fromPairs.ts
+++ src/fromPairs.ts
@@ -3,6 +3,8 @@ import { getLastSemigroup } from "fp-ts/lib/Semigroup";
 import { array } from "fp-ts/lib/Array";
 import { identity } from "fp-ts/lib/function";
 
-export function fromPairs<A, K extends string>(as: Array<[K, A]>) {
+export function fromPairs<A, K extends string>(
+  as: Array<[K, A]>
+): Record<K, A> {
   return fromFoldableMap(getLastSemigroup<A>(), array)(as, identity);
 }
