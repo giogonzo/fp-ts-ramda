e0042b73e840fbf019b9271f22cf6012ac41905b
Harry Kightlinger <kightlingerh@gmail.com>
2019-11-20 07:11:18 -0800

    Removed unneccessary _add function

--- src/add.ts
+++ src/add.ts
@@ -1,10 +1,6 @@
 import { fieldNumber } from 'fp-ts/lib/Field';
 import { Endomorphism } from 'fp-ts/lib/function';
 
-function _add(x: number, y: number): number {
-  return fieldNumber.add(x, y);
-}
-
 /**
  * Same as [R.add](https://ramdajs.com/docs/#add). Adds two values
  *
@@ -15,8 +11,8 @@ export function add(x: number): Endomorphism<number>;
 export function add(x: number, y: number): number;
 export function add(x: number, y?: number): Endomorphism<number> | number {
   if (y === undefined) {
-    return y => _add(x, y);
+    return y => fieldNumber.add(x, y);
   } else {
-    return _add(x, y);
+    return fieldNumber.add(x, y);
   }
 }
